1151a698f6c047970c29b6be24da98c4578adc10
Krikod <kristen.kod@gmail.com>
2021-03-12 15:02:56 +0100

    Make Controller pubf show product + twig

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,11 @@ class ProductController extends AbstractController
 {
 	/**
 	 * @Route("/{slug}", name="product_category")
+	 *
+	 * @param $slug
+	 * @param CategoryRepository $repository
+	 *
+	 * @return Response
 	 */
     public function category($slug, CategoryRepository $repository): Response
     {
@@ -20,8 +26,34 @@ class ProductController extends AbstractController
     		'slug' => $slug
 	    ]);
 
+    	if (!$category) {
+    		throw $this->createNotFoundException("La catégorie demandée n'existe pas.");
+	    }
+
         return $this->render('product/category.html.twig', [
             'category' => $category,
         ]);
+    }
+
+	/**
+	 * @Route("/{category_slug}/{slug}", name="product_show")
+	 *
+	 * @param $slug
+	 * @param ProductRepository $repository
+	 *
+	 * @return Response
+	 */
+	public function show($slug, ProductRepository $repository): Response {
+		$product = $repository->findOneBy([
+			'slug' => $slug
+		]);
+
+		if (!$product) {
+			throw $this->createNotFoundException("Le produit demandé n'existe pas.");
+		}
+
+		return $this->render('product/show.html.twig', [
+			'product' => $product
+		]);
     }
 }
