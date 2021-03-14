cbfd00367037c15a32b9245e931f1fc805b52431
Krikod <kristen.kod@gmail.com>
2021-03-14 12:22:49 +0100

    form builder + form view with form_themes config

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -54,6 +60,47 @@ class ProductController extends AbstractController
 
 		return $this->render('product/show.html.twig', [
 			'product' => $product
+		]);
+    }
+
+	/**
+	 * @Route("/admin/produit/ajouter", name="product_create")
+	 * @param FormFactoryInterface $factory
+	 */
+	public function create(FormFactoryInterface $factory) {
+		$builder = $factory->createBuilder();
+		$builder
+			->add('name', TextType::class, [
+				'label' => 'Nom du produit',
+				'attr' => [
+					'placeholder' => 'Tapez le nom du produit'
+				]
+			])
+		->add('shortDescription', TextareaType::class, [
+			'label' => 'Description courte',
+			'attr' => [
+				'placeholder' => 'Tapez une description courte du produit'
+			]
+		])
+			->add('price', MoneyType::class, [
+				'label' => 'Prix',
+				'attr' => [
+					'placeholder' => 'Tapez le prix du produit'
+				]
+			])
+			->add('category', EntityType::class, [
+				'label' => 'Catégorie',
+				'placeholder' => '-- Choisir une catégorie --',
+				'class' => Category::class,
+				'choice_label' => function(Category $category) {
+				return strtoupper($category->getName());
+				}
+			]);
+
+		$formView = $builder->getForm()->createView();
+
+		return $this->render('product/create.html.twig', [
+			'formView' => $formView
 		]);
     }
 }
