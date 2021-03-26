860e35b9dbeb9f5f985b7272bb25c49ea3f84a48
Krikod <kristen.kod@gmail.com>
2021-03-26 19:38:50 +0100

    Control over login error messages with Symfony/translation

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\LoginType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+	/**
+	 * @Route("/login", name="security_login")
+	 */
+    public function login(AuthenticationUtils $utils): Response
+    {
+    	$form = $this->createForm( LoginType::class, ['email' => $utils->getLastUsername()]);
+
+    	$formView = $form->createView();
+
+        return $this->render('security/login.html.twig', [
+        	'formView' => $formView,
+	        'error' => $utils->getLastAuthenticationError()
+        ]);
+    }
+}
