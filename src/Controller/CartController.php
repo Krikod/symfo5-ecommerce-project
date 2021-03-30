6c8d141f1b7f1d13cc1dbb453a116a5ab251d682
Krikod <kristen.kod@gmail.com>
2021-03-30 17:16:18 +0200

    session: save product in cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,18 @@ class CartController extends AbstractController
 	/**
 	 * @Route("/panier/ajouter/{id}", name="cart_add")
 	 */
-    public function add(): Response
+    public function add($id, Request $request): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        $cart = $request->getSession()->get( 'cart', []);
+
+        if (array_key_exists( $id, $cart)) {
+        	$cart[$id]++;
+        } else {
+        	$cart[$id] = 1;
+        }
+
+        $request->getSession()->set( 'cart', $cart);
+//	    $request->getSession()->remove( 'cart');
+//	    dd( $request->getSession()->get( 'cart'));
     }
 }
