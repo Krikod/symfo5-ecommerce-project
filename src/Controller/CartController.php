881d3d0f5d0d21ffa307db30bb91d6153a54943f
Krikod <kristen.kod@gmail.com>
2021-03-30 17:37:56 +0200

    cart securization

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
 	/**
-	 * @Route("/panier/ajouter/{id}", name="cart_add")
+	 * @Route("/panier/ajouter/{id}", name="cart_add",
+	 *     requirements={"id":"\d+"})
 	 */
-    public function add($id, Request $request): Response
+    public function add($id, Request $request, ProductRepository $repo): Response
     {
+    	$product = $repo->find( $id);
+    	if (!$product) {
+    		throw $this->createNotFoundException("Le produit $id n'existe pas");
+	    }
         $cart = $request->getSession()->get( 'cart', []);
 
         if (array_key_exists( $id, $cart)) {
@@ -25,5 +31,10 @@ class CartController extends AbstractController
         $request->getSession()->set( 'cart', $cart);
 //	    $request->getSession()->remove( 'cart');
 //	    dd( $request->getSession()->get( 'cart'));
+
+	    return $this->redirectToRoute( 'product_show', [
+	    	'category_slug' => $product->getCategory()->getSlug(),
+		    'slug' => $product->getSlug()
+	    ])
     }
 }
