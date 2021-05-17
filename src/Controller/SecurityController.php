0f1a0b7c8501d9ef46a0a1bd9641a4a6709da720
Krikod <kristen.kod@gmail.com>
2021-05-17 11:44:59 +0200

    Create Twig filter |amount

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,6 +15,7 @@ class SecurityController extends AbstractController
 	 */
     public function login(AuthenticationUtils $utils): Response
     {
+    	// todo login ne laisse pas à la page visitée
     	$form = $this->createForm( LoginType::class, ['email' => $utils->getLastUsername()]);
 
 //    	$formView = $form->createView();
